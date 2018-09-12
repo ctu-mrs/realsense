4a8f4556b41f29000e0bedd33f430d15b35c0eaf
uav10 <uav10@mrs.felk.cvut.cz>
2018-09-12 19:04:12 +0200

    Ready for orb slam dataset

---
+++ scripts/orb_slam_dataset.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rosrun dynamic_reconfigure dynparam set /$UAV_NAME/rs_d435/realsense2_camera_manager rs435_depth_enable_auto_exposure 0
+rosrun dynamic_reconfigure dynparam set /$UAV_NAME/rs_d435/realsense2_camera_manager rs435_depth_enable_auto_exposure 1
+run dynamic_reconfigure dynparam set /$UAV_NAME/rs_d435/realsense2_camera_manager rs435_depth_emitter_enabled 0
+
