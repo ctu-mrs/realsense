49c88f58ef7b25b21754962a989bdfff7622f636
Jakub Lenner <lennejak@fel.cvut.cz>
2025-05-29 10:24:56 +0200

    updated Dockerfile and added compose file

---
+++ docker/ros_entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+. /opt/ros/jazzy/setup.sh
+#. /home/ubuntu/ws/install/setup.sh
+export UAV_NAME=uav1
+#cd /home/ubuntu/ws
+ros2 launch mrs_realsense uav.launch.py $1
+
+# docker run --privileged -it -v ./custom_config.yaml:/home/ubuntu/ws/custom_config.yaml mrs_realsense:latest custom_config:=custom_config.yaml
