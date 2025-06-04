67203e8f558e1ebe14bbf87b309c7c2bd8646f63
Tomas Baca <Tomas Baca>
2025-06-04 11:10:11 +0200

    changes from tomas

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
 . /opt/ros/jazzy/setup.sh
+
 #. /home/ubuntu/ws/install/setup.sh
-export UAV_NAME=uav1
+
+if [-z UAV_NAME ]; then
+  echo "$0: the UAV_NAME environment variable is not set"
+  exit 1
+fi
+
 #cd /home/ubuntu/ws
-ros2 launch mrs_realsense uav.launch.py $1
+ros2 launch mrs_realsense uav.launch.py $@
 
 # docker run --privileged -it -v ./custom_config.yaml:/home/ubuntu/ws/custom_config.yaml mrs_realsense:latest custom_config:=custom_config.yaml
