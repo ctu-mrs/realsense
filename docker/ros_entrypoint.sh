537afa30799be299fd895c51e9fd5d0c8d54659a
Filip <4060824+filipopo@users.noreply.github.com>
2026-07-14 14:29:27 +0200

    set ROS_DISTRO in the container entrypoint if it's missing

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-. /opt/ros/$ROS_DISTRO/setup.sh
+# set ROS_DISTRO if it's missing
+if [ -z "$ROS_DISTRO" ]; then
+  for dir in /opt/ros/*; do
+    if [ -f "$dir/setup.bash" ]; then
+      export ROS_DISTRO=$(basename "$dir")
+    fi
+  done
+fi
 
-#. /home/ubuntu/ws/install/setup.sh
+source /opt/ros/$ROS_DISTRO/setup.bash
 
 if [-z UAV_NAME ]; then
   echo "$0: the UAV_NAME environment variable is not set"
   exit 1
 fi
 
-#cd /home/ubuntu/ws
 ros2 launch mrs_realsense uav.launch.py $@
 
 # docker run --privileged -it -v ./custom_config.yaml:/home/ubuntu/ws/custom_config.yaml ctumrs/realsense:latest custom_config:=custom_config.yaml
